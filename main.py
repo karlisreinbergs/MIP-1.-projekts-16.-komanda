7ea6b0e1c99de6cc3b4ecd1e832dd4339ab3af8f
Karlis <karlis.reinbergs@gmail.com>
2025-03-25 22:57:29 +0200

    komentari

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import random
 #           print(f"Dators uzvar ar {datora_rez} punktiem")
 #
 def virknes_izveide(garums):
-      return [random.choice([1, 2, 3, 4]) for num in range(garums)]
+      return [random.choice([1, 2, 3, 4]) for num in range(garums)]           #izveido random virnki
 
 
-start_state = GameState(
+start_state = GameState(                                                      #izveido speles gajiena datu strukturu
       virkne = virknes_izveide(15),
       cilveka_rez = 0,
       datora_rez = 0,
@@ -25,17 +25,17 @@ start_state = GameState(
 
 current_state = start_state
 
-datora_algo = input("izvelies datora algoritmu: ")
+#datora_algo = input("izvelies datora algoritmu: ")
 
 while (not current_state.tuksa_virkne()):
       print(current_state)
 
-      if (current_state.ir_cilveka_gajiens):
-            index = int(input("ievadi indeksu: "))
-            if (index == 9): exit()
-            darbiba = int(input("izvelies 1, 2, vai 3: "))
-            current_state = gajiena_izpilde2(current_state, index, darbiba)
+      if (current_state.ir_cilveka_gajiens):                                        #parbauda vai ir cilveka gajiens
+            index = int(input("ievadi indeksu: "))                                  #ievadi indeksu skaitlim kuru gribi izmantot
+            if (index == 9): exit()                                                 #vnk lai izbeigtu kodu
+            darbiba = int(input("izvelies 1, 2, vai 3: "))                          #izvelies kuru ricibu velies veikt ar skaitli, 1 ir pieskaitit sev, 2 ir sadalit vieniniekos, un 3 ir 4 sadalits divniekos
+            current_state = gajiena_izpilde2(current_state, index, darbiba)         #palaiz gajienu funkciju cilvekam
       else:
-            current_state.generate_children()
-            current_state = current_state.children[0] 
+            current_state.generate_children()                                       #genere iespejamos gajiena variantus
+            current_state = current_state.children[0]                               #sobrid nav nekads algoritms tapec izvelas pirmo iespeju
 
